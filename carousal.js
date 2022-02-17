afab3969358f8a9761d10f73d904700c77c8f325
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-02-17 20:28:18 +0530

    feat: carousel

---
+++ carousal.js
@@ -0,0 +1,47 @@
+let r1 = document.getElementById("radio1");
+let r2 = document.getElementById("radio2");
+let r3 = document.getElementById("radio3");
+let r4 = document.getElementById("radio4");
+
+console.log("hey");
+let label = document.querySelectorAll(".manual-btn");
+
+// function clearManualBtnStyle() {
+//   label.forEach((manualBtn) => {
+//     manualBtn.style.backgroundColor = "transparent";
+//   });
+// }
+
+// label.forEach((manualBtn) => {
+//   manualBtn.addEventListener("click", () => {
+//     // manualBtn.classList.add("active");
+//     clearManualBtnStyle();
+//     manualBtn.style.backgroundColor = "var(--primary-color)";
+//   });
+// });
+
+function clearManualBtnStyle() {
+  label.forEach((manualBtn) => {
+    // manualBtn.style.backgroundColor = "transparent";
+    manualBtn.classList.remove("active");
+  });
+}
+
+// label.forEach((manualBtn) => {
+//   manualBtn.addEventListener("click", () => {
+//     // manualBtn.classList.add("active");
+//     clearManualBtnStyle();
+//     manualBtn.style.backgroundColor = "var(--primary-color)";
+//   });
+// });
+
+let manualBtns = document.querySelector(".manual-navigation");
+
+manualBtns.addEventListener("click", (e) => {
+  //   removeBg();
+  clearManualBtnStyle();
+  if (e.target.className == "manual-btn") {
+    // e.target.style.backgroundColor = "var(--primary-color)";
+    e.target.classList.add("active");
+  }
+});
